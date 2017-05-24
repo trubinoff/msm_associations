e0d55482bfc03a50ba9931c2cc3b820a8d15e9b6
Tracey Rubinoff <trubinoff@chicagobooth.edu>
2017-05-23 23:22:16 -0500

    End of Day Wednesday Changes

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,12 +1,24 @@
 class Movie < ApplicationRecord
- # - director_id: must be present
- validates :director_id, :presence => true
- # - title: must be present; must be unique in combination with year
- validates :title, :presence => true, :uniqueness => {:scope => [:year, :duration]}
- # - year: must be integer between 1870 and 2050
-validates :year, numericality: { only_integer: true, greater_than_or_equal_to: 1870, less_than_or_equal_to: 2050 }
- # - duration: must be integer between 0 and 2764800, but it can be blank (hint: there is an option `:allow_blank => true`)
- validates :duration, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 2764800 }
- # - description: no rules
- # - image_url: no rules
+  # - director_id: must be present
+  validates :director_id, :presence => true
+  # - title: must be present; must be unique in combination with year
+  validates :title, :presence => true, :uniqueness => {:scope => [:year, :duration]}
+  # - year: must be integer between 1870 and 2050
+  validates :year, numericality: { only_integer: true, greater_than_or_equal_to: 1870, less_than_or_equal_to: 2050 }
+  # - duration: must be integer between 0 and 2764800, but it can be blank (hint: there is an option `:allow_blank => true`)
+  validates :duration, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 2764800 }
+  # - description: no rules
+  # - image_url: no rules
+
+  belongs_to(:director, :class_name => "Director", :foreign_key => "director_id")
+
+  has_many(:characters, :class_name => "Character", :foreign_key => "movie_id")
+
+  has_many :characters
+  has_many :actors, :through => :characters
+
+
+
+
+
 end
